aa7ea2c2752592c7ac09de7fd30221c673a9e82e
vc64 <vchen2022@spyponders.com>
2022-02-26 13:17:06 -0500

    testing json

--- mapDataGen.py
+++ mapDataGen.py
@@ -1,15 +1,35 @@
 import pandas as pd
 import numpy as np
 import random
+import json
 
-open_path = "50m.tsv"
-df = pd.read_csv(open_path, sep="\t")   
+open_path1 = "50m.tsv"
+df1 = pd.read_csv(open_path1, sep="\t")   
 
-print(df)
+open_path2 = "gdp_total_billion_2020.csv"
+df2 = pd.read_csv(open_path2, sep=",")   
 
+
+print(df1["iso_n3"], df1["sovereignt"])
+
+# # print(df2["Country Name"])
+
+# tmp = pd.Series(list(set(df1["sovereignt"]).symmetric_difference(set(df2["Country Name"]))))
+
+# print(tmp)
+
+f = open('50m.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+print(data["objects"]["countries"]["properties"])
+
+"""
 timedata = pd.DataFrame({})
 
-for i in range(10):
+for i in range(1, 11):
 
     curr_arr = []
     for c in range(241):
@@ -30,3 +50,5 @@ print(out)
 # out = pd.DataFrame(np.random.randint(0,10,(200,100)))
 out_path = "edited_50m.tsv"
 out.to_csv(out_path, index=False, sep="\t")
+
+"""
